410e9589cc71995d56fc1fe78d95daeac50b092f
Marcos Nolasco <marcosabn@gmail.com>
2021-11-29 13:14:25 -0300

    started project

---
+++ app/Controllers/Http/MedicosController.ts
@@ -0,0 +1,7 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class MedicosController {
+  public index() {
+    return ['medico 1', 'medico 2', 'medico 3']
+  }
+}
